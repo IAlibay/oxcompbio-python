e709082596d1bd630ff2301f82b2b21c330bbf8b
IAlibay <ialibay@mdanalysis.org>
2022-12-12 08:57:50 +0000

    Add ipywidgets pin

--- setup-conda.sh
+++ setup-conda.sh
@@ -5,7 +5,7 @@ conda create -y -n OxPython -c conda-forge mamba python=3.9
 
 source activate OxPython
 
-mamba install -y -c jaimergp/label/unsupported-cudatoolkit-shim -c conda-forge notebook jupyter_contrib_nbextensions numpy matplotlib "MDAnalysis>=2.0.0" nglview openmm parmed
+mamba install -y -c jaimergp/label/unsupported-cudatoolkit-shim -c conda-forge notebook jupyter_contrib_nbextensions numpy matplotlib "MDAnalysis>=2.0.0" nglview openmm parmed "ipywidgets<8"
 
 jupyter-nbextension enable nglview --py --sys-prefix
 
