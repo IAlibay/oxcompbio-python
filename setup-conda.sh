aae2ec439a968bf68c21d821c987cc39aa8c8dbb
IAlibay <ialibay@mdanalysis.org>
2022-12-12 08:27:07 +0000

    Add setup-conda script

---
+++ setup-conda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create conda environment
+conda env create -f environment.yaml
+source activate OxPython
+
+jupyter-nbextension enable nglview --py --sys-prefix
+
+# Enable Exercise 2 extension
+# https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/nbextensions/exercise2/readme.html
+jupyter contrib nbextension install --user
+jupyter nbextension enable exercise2/main
+
+echo "setup done"
